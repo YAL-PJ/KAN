f43102bbf9be2dc717dca68cdb9e2bb5b148dd5e
YAL-PJ <115693590+YAL-PJ@users.noreply.github.com>
2026-05-17 09:48:06 +0300

    Fix debug build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,11 @@ android {
         compose = true
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -32,6 +37,7 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.foundation:foundation-layout")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
